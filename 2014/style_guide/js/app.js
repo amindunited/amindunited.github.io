041ae00eeea81ba51bec5ce3bda03573c13fae87
amindunited <buckley.robin@gmail.com>
2014-11-06 13:51:29 +1100

    Break iCheck

--- 2014/style_guide/js/app.js
+++ 2014/style_guide/js/app.js
@@ -1,4 +1,11 @@
 /** Copyright Robin Buckley. 2014 **/
 $(function(){
 	console.log("has begun");
+
+    $('input').iCheck({
+        checkboxClass: 'icheckbox_square-grey',
+        radioClass: 'iradio_square-grey',
+        increaseArea: '20%' // optional
+    });
+
 });

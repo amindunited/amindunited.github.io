f91610a00a7b2c9a1cc3ae0c7c1019dd97e6b94f
amindunited <buckley.robin@gmail.com>
2020-08-09 00:08:49 +1000

    Deleted old (empty) dirs

--- 2014/style_guide/js/app.js
+++
@@ -1,8 +0,0 @@
-/** Copyright Robin Buckley. 2014 **/
-$(function(){
-    $('input').iCheck({
-        checkboxClass: 'icheckbox_square-grey',
-        radioClass: 'iradio_square-grey',
-        increaseArea: '20%' // optional
-    });
-});
--- 2014/style_guide/src/index.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-    $('input').iCheck({
-        checkboxClass: 'icheckbox_square-grey',
-        radioClass: 'iradio_square-grey',
-        increaseArea: '20%' // optional
-    });
-});
